b2cb699355024a1f23d2adeb49adf08dd028a3db
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2021-09-27 20:15:23 -0300

    Mercado Liebre

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+/* Configuracion Extra para Ruta de Imagenes Public */
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.listen (3000, () => console.log('Procesando LocalHost 3000'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'))
+});
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'))
+});
+
+app.post('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
+});
+
+app.post('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
+});
